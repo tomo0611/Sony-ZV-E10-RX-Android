f4adadd4aae9c8e3edf40da4e7e0403f2f259adb
tomo0611 <tomo0611@hotmail.com>
2024-08-16 21:43:35 +0900

    修正

--- app/src/main/java/jp/tomo0611/sony_zv_e10/packet/InitEventRequestPacket.kt
+++ app/src/main/java/jp/tomo0611/sony_zv_e10/packet/InitEventRequestPacket.kt
@@ -3,8 +3,15 @@ package jp.tomo0611.sony_zv_e10.packet
 import jp.tomo0611.sony_zv_e10.enum.PacketType
 import java.nio.ByteBuffer
 
-class InitEventRequestPacket(val mConnectionNumber: Int) :
+class InitEventRequestPacket(connectionNumber: Int) :
     AbstractPacket(4, PacketType.InitEventRequestPacket) {
+
+    var mConnectionNumber: Int
+
+    init {
+        this.mConnectionNumber = connectionNumber
+    }
+
     override val bytes: ByteBuffer
         get() {
             val bytes = super.bytes
